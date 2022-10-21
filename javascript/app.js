2e5c222477a71d23b86c4e18733600eb979fdcfa
Tristan <tristanthorburn@gmail.com>
2022-10-21 17:56:07 -0400

    Mobile JS done and eggy

--- javascript/app.js
+++ javascript/app.js
@@ -1,30 +1,36 @@
-// Project Accordion
-const projects = {}
+// Mobile Hamburger
+const hamburger = {};
 
-projects.container = document.querySelectorAll(".project-container");
+hamburger.button = document.querySelector(".hamburger");
 
-projects.accordion = () => {
-    projects.container.forEach(project =>{
-        project.addEventListener("click", (e) => {
-            project.classList.toggle("active")
-            console.log(e.target);
-            
-            const projectDetails = e.target.parentNode.parentElement.nextElementSibling;
-            if(e.target.parentNode.parentElement.classList.contains("active")){
-                projectDetails.style.maxHeight = projectDetails.scrollHeight + "px";
-            }
-            else{
-                projectDetails.style.maxHeight = 0;
-            }
-        });
-    });
-};
+hamburger.showMenu = () => {
+    hamburger.button.addEventListener("click", () => {
+        const menu = document.querySelector(".desktop-nav");
 
-projects.init = () => {
-    projects.accordion();
-};
+        menu.classList.toggle("active");
+        hamburger.button.classList.toggle("active");
+    })
+}
 
-projects.init();
+hamburger.hideMenu = () => {
+    const closeMenu = document.querySelectorAll(".menu-link");
+
+    closeMenu.forEach(link => {
+        link.addEventListener("click", () => {
+            const menu = document.querySelector(".desktop-nav");
+            menu.classList.remove("active");
+            hamburger.button.classList.remove("active");
+        })
+    })
+}
+
+hamburger.init = () => {
+    hamburger.showMenu();
+    hamburger.hideMenu();
+}
+
+hamburger.init();
+
 // Tech section slider
 const tech = {};
 
@@ -53,3 +59,85 @@ tech.init = () => {
 };
 
 tech.init();
+
+// Project Accordion
+const projects = {};
+
+projects.container = document.querySelectorAll(".project-container");
+
+projects.accordion = () => {
+    projects.container.forEach(project =>{
+        project.addEventListener("click", (e) => {
+            project.classList.toggle("active")
+            
+            const projectDetails = e.target.parentNode.parentElement.nextElementSibling;
+            if(e.target.parentNode.parentElement.classList.contains("active")){
+                projectDetails.style.maxHeight = projectDetails.scrollHeight + "px";
+            }
+            else{
+                projectDetails.style.maxHeight = 0;
+            }
+        });
+    });
+};
+
+projects.init = () => {
+    projects.accordion();
+};
+
+projects.init();
+
+// Eggy
+
+const eggy = {};
+
+eggy.secret = document.querySelector(".secret");
+
+eggy.cracked = () => {
+    eggy.secret.addEventListener("dblclick", () => {
+        const proj = document.querySelector(".eggy");
+        eggy.secret.classList.toggle("active");
+        proj.classList.toggle("active");
+
+        if(proj.classList.contains("active")){
+            proj.style.maxHeight = proj.scrollHeight + "px";
+        }
+        else{
+            proj.style.maxHeight = 0;
+        }
+    })
+}
+
+eggy.init = () => {
+    eggy.cracked();
+}
+
+eggy.init();
+
+// Contact Accordion
+const contact = {};
+
+contact.container = document.querySelector(".contact");
+
+contact.accordion = () => {
+    contact.container.addEventListener("click", () => {
+        contact.h2 = document.getElementById("contact");
+        contact.form = document.querySelector(".contact-form");
+
+        contact.h2.classList.toggle("active");
+
+        if(contact.h2.classList.contains("active")){
+            contact.form.style.maxHeight = contact.form.scrollHeight + "px";
+        }
+        else{
+            contact.form.style.maxHeight = 0;
+        }
+    });
+};
+
+contact.init = () => {
+    contact.accordion();
+}
+
+contact.init();
+
